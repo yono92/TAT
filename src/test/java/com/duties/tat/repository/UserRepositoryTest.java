1baf23ddfbd838d85a4588266e198dbb9b194125
yoonsejong <oxwk123@gmail.com>
2024-03-16 23:32:28 +0900

    [feat] User Password 암호화

--- src/test/java/com/duties/tat/repository/UserRepositoryTest.java
+++ src/test/java/com/duties/tat/repository/UserRepositoryTest.java
@@ -1,13 +1,17 @@
 package com.duties.tat.repository;
 
 import com.duties.tat.entity.User;
+import com.duties.tat.service.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
@@ -17,11 +21,15 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@DataJpaTest
+@SpringBootTest
 class UserRepositoryTest {
 
     @Autowired
     private  UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
 
     // 유저 생성 테스트
     @Test
@@ -32,10 +40,10 @@ class UserRepositoryTest {
         user.setId("test");
         user.setPassword("test");
         user.setName("test");
-        user.setEmail("test");
-        user.setPhone("test");
-        user.setRole("test");
-        userRepository.save(user);
+        user.setEmail ( "test" );
+        user.setPhone ( "test" );
+        user.setRole ( "test" );
+        userService.save(user);
     }
 
     // 유저정보 전체 가져오는 테스트코드
